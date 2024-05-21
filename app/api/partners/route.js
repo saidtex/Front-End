e4cfbf647998980a76f55f9380985cb2d89bb823
saidtex6 <168695206+saidtex6@users.noreply.github.com>
2024-05-21 13:44:12 +0100

    Front-End

--- app/api/partners/route.js
+++ app/api/partners/route.js
@@ -1,22 +1,9 @@
-import { connectMongoDB } from "../../../libs/mongodb";
+import connectMongoDB from "../../../libs/mongodb";
 import Partner from "../../../models/partner";
+import { NextResponse } from "next/server";
 
-// Connect to MongoDB
-await connectMongoDB();
-
-export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    try {
-      const partners = await Partner.find();
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-      return res.json(partners);
-    } catch (error) {
-      console.error("Error fetching partners:", error);
-      return res.status(500).json({ message: 'Internal Server Error' });
-    }
-  } else {
-    return res.status(405).json({ message: 'Method Not Allowed' });
-  }
+export async function GET() {
+  await connectMongoDB();
+  const partners = await Partner.find();
+  return NextResponse.json( partners );
 }
