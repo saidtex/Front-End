300487dc9f8154618bcb954418e1f4922330c421
saidtex6 <168695206+saidtex6@users.noreply.github.com>
2024-05-21 13:17:37 +0100

    Front-End

--- app/api/partners/route.js
+++ app/api/partners/route.js
@@ -1,9 +1,29 @@
-import connectMongoDB from "../../../libs/mongodb";
+import nextConnect from 'next-connect';
+import { connectMongoDB } from "../../../libs/mongodb";
 import Partner from "../../../models/partner";
-import { NextResponse } from "next/server";
 
-export async function GET() {
-  await connectMongoDB();
-  const partners = await Partner.find();
-  return NextResponse.json( partners );
-}
+const handler = nextConnect();
+
+// Connect to MongoDB
+handler.use(connectMongoDB);
+
+// Add CORS middleware
+handler.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  next();
+});
+
+// Define your API route handler
+handler.get(async (req, res) => {
+  try {
+    const partners = await Partner.find();
+    return res.json(partners);
+  } catch (error) {
+    console.error("Error fetching partners:", error);
+    return res.status(500).json({ message: 'Internal Server Error' });
+  }
+});
+
+export default handler;
