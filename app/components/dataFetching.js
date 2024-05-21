10abe579ec3f956dd4e9a8282b2612d3ba1b8cfb
saidtex6 <168695206+saidtex6@users.noreply.github.com>
2024-05-21 17:38:16 +0100

    Update dataFetching.js

--- app/components/dataFetching.js
+++ app/components/dataFetching.js
@@ -1,12 +1,25 @@
+// dataFetching.js
 export async function getServerSideProps() {
-  const timestamp = new Date().getTime(); // Get current timestamp
-  const url = `https://saidtex.ma/api/partners?timestamp=${timestamp}`; // Append timestamp as a query parameter
-  const response = await fetch(url, { cache: "no-store" });
-  const blogs = await response.json();
+  try {
+    const timestamp = new Date().getTime(); // Get current timestamp
+    const url = `https://saidtex.ma/api/partners?timestamp=${timestamp}`; // Append timestamp as a query parameter
+    const response = await fetch(url, { cache: "no-store" });
+    if (!response.ok) {
+      throw new Error("Failed to fetch blogs");
+    }
+    const blogs = await response.json();
 
-  return {
-    props: {
-      initialGalleryItems: blogs,
-    },
-  };
+    return {
+      props: {
+        initialGalleryItems: blogs,
+      },
+    };
+  } catch (error) {
+    console.error("Error in getServerSideProps:", error);
+    return {
+      props: {
+        initialGalleryItems: [],
+      },
+    };
+  }
 }
