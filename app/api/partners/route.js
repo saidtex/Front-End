1dc29d11939fb329a93bc168165342b82ea199b1
saidtex6 <168695206+saidtex6@users.noreply.github.com>
2024-05-21 17:19:10 +0100

    Update route.js

--- app/api/partners/route.js
+++ app/api/partners/route.js
@@ -2,10 +2,15 @@ import connectMongoDB from "../../../libs/mongodb";
 import Partner from "../../../models/partner";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(request) {
   await connectMongoDB();
   const partners = await Partner.find();
 
-  // Set Cache-Control header to prevent caching
-  return NextResponse.json(partners, { headers: { 'Cache-Control': 'no-store' } });
+  const response = NextResponse.json(partners);
+  response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  response.headers.set('Pragma', 'no-cache');
+  response.headers.set('Expires', '0');
+  response.headers.set('Surrogate-Control', 'no-store');
+  
+  return response;
 }
