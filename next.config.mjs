bd9178f3093432e85f8ccfcf9e6169c9f5aa18d5
saidtex6 <168695206+saidtex6@users.noreply.github.com>
2024-05-21 19:29:56 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/:path*", // Adjust this according to your API route pattern
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-cache, no-store, must-revalidate",
+          },
+        ],
+      },
+      {
+        source: "/static/:path*", // Adjust this according to your static asset path pattern
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=3600", // Example: cache static assets for 1 hour
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
+
