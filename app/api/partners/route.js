524bb4c056dfe5b73c41b58a92a7c62e6a2ea9be
saidtex6 <168695206+saidtex6@users.noreply.github.com>
2024-05-21 17:07:32 +0100

    Update route.js

--- app/api/partners/route.js
+++ app/api/partners/route.js
@@ -5,6 +5,7 @@ import { NextResponse } from "next/server";
 export async function GET() {
   await connectMongoDB();
   const partners = await Partner.find();
+  res.setHeader('Cache-Control', 'no-store');
 
   // Set Cache-Control header to prevent caching
   return NextResponse.json(partners, {
