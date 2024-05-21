2b0d4af642e53987edd33a0785ea5db7fef05e96
saidtex6 <168695206+saidtex6@users.noreply.github.com>
2024-05-21 14:06:32 +0100

    Front-End

--- app/api/partners/route.js
+++ app/api/partners/route.js
@@ -1,18 +1,25 @@
 import connectMongoDB from "../../../libs/mongodb";
 import Partner from "../../../models/partner";
-import { NextResponse } from "next/server";
 
-export async function GET() {
+const PartnersPage = ({ partners }) => {
+  // Render your component or page with the fetched data
+};
+
+export async function getServerSideProps() {
   await connectMongoDB();
   const partners = await Partner.find();
-  
-  // Create a response with CORS headers
-  const response = NextResponse.json(partners);
-  
-  // Set the CORS headers
-  response.headers.set("Access-Control-Allow-Origin", "*");
-  response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  
-  return response;
+
+  // Set CORS headers in the response
+  return {
+    props: {
+      partners: JSON.parse(JSON.stringify(partners))
+    },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers": "Content-Type, Authorization"
+    }
+  };
 }
+
+export default PartnersPage;
