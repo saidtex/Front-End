2f3efabf4e90b30dddecc255ded4323b62f77eb8
saidtex6 <168695206+saidtex6@users.noreply.github.com>
2024-05-21 13:27:47 +0100

    Front-End

--- app/api/partners/route.js
+++ app/api/partners/route.js
@@ -1,29 +1,22 @@
-import nextConnect from 'next-connect';
 import { connectMongoDB } from "../../../libs/mongodb";
 import Partner from "../../../models/partner";
 
-const handler = nextConnect();
-
 // Connect to MongoDB
-handler.use(connectMongoDB);
+await connectMongoDB();
 
-// Add CORS middleware
-handler.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  next();
-});
-
-// Define your API route handler
-handler.get(async (req, res) => {
-  try {
-    const partners = await Partner.find();
-    return res.json(partners);
-  } catch (error) {
-    console.error("Error fetching partners:", error);
-    return res.status(500).json({ message: 'Internal Server Error' });
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    try {
+      const partners = await Partner.find();
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+      return res.json(partners);
+    } catch (error) {
+      console.error("Error fetching partners:", error);
+      return res.status(500).json({ message: 'Internal Server Error' });
+    }
+  } else {
+    return res.status(405).json({ message: 'Method Not Allowed' });
   }
-});
-
-export default handler;
+}
