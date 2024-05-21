71177e3e82610cc5fcadc06149c62b0bb86108c2
saidtex6 <168695206+saidtex6@users.noreply.github.com>
2024-05-21 17:40:30 +0100

    Update dataFetching.js

--- app/components/dataFetching.js
+++ app/components/dataFetching.js
@@ -2,12 +2,14 @@
 export async function getServerSideProps() {
   try {
     const timestamp = new Date().getTime(); // Get current timestamp
-    const url = `https://saidtex.ma/api/partners?timestamp=${timestamp}`; // Append timestamp as a query parameter
+    const url = `https://saidtex.ma/api/partners`; // Append timestamp as a query parameter
     const response = await fetch(url, { cache: "no-store" });
     if (!response.ok) {
       throw new Error("Failed to fetch blogs");
     }
     const blogs = await response.json();
+
+    console.log("Fetched blogs:", blogs); // Log fetched data
 
     return {
       props: {
