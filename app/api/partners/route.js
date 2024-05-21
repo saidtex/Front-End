4754042089d2c7dc16eaac037ca555d8c4cb6d91
saidtex6 <168695206+saidtex6@users.noreply.github.com>
2024-05-21 14:19:54 +0100

    Front-End

--- app/api/partners/route.js
+++ app/api/partners/route.js
@@ -1,30 +1,9 @@
-// pages/index.js
-
-import Hero1 from "../../components/partenaires.jsx";
 import connectMongoDB from "../../../libs/mongodb";
 import Partner from "../../../models/partner";
+import { NextResponse } from "next/server";
 
-const HomePage = ({ initialGalleryItems }) => {
-  return <Hero1 initialGalleryItems={initialGalleryItems} />;
-};
-
-export async function getServerSideProps() {
+export async function GET() {
   await connectMongoDB();
   const partners = await Partner.find();
-
-  const props = {
-    initialGalleryItems: JSON.parse(JSON.stringify(partners))
-  };
-
-  // Set CORS headers in the response
-  return {
-    props,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-      "Access-Control-Allow-Headers": "Content-Type, Authorization"
-    }
-  };
+  return NextResponse.json( partners );
 }
-
-export default HomePage;
