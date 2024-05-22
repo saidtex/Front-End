7df745f9e8622f8c60f07efda9c1224d3a582386
saidtex6 <168695206+saidtex6@users.noreply.github.com>
2024-05-22 02:39:39 +0100

    Update partenaires.jsx

--- app/components/partenaires.jsx
+++ app/components/partenaires.jsx
@@ -17,7 +17,11 @@ const Hero1 = () => {
 
   const fetchBlogs = async () => {
     try {
-      const response = await fetch("https://saidtex.ma/api/partners", { cache: 'force-cache' });
+      const response = await fetch("https://saidtex.ma/api/partners", {
+    next: {
+      revalidate: 60, // 1 hour
+    },
+  });
       if (!response.ok) {
         throw new Error("Failed to fetch blogs");
       }
