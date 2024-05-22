4901fb6341041c0600f07b7057f179dbb7473c57
saidtex6 <168695206+saidtex6@users.noreply.github.com>
2024-05-22 02:50:43 +0100

    Update route.js

--- app/api/partners/route.js
+++ app/api/partners/route.js
@@ -5,5 +5,13 @@ import { NextResponse } from "next/server";
 export async function GET() {
   await connectMongoDB();
   const partners = await Partner.find();
-  return NextResponse.json( partners );
+
+  return new Response(JSON.stringify(partners), {
+    status: 200,
+    headers: {
+      'Cache-Control': 'public, s-maxage=1',
+      'CDN-Cache-Control': 'public, s-maxage=60',
+      'Vercel-CDN-Cache-Control': 'public, s-maxage=3600',
+    },
+  });
 }
