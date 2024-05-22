578446ddc43f3e7f0962ff9036df14a1a1478911
saidtex6 <168695206+saidtex6@users.noreply.github.com>
2024-05-22 02:22:51 +0100

    Update route.js

--- app/api/partners/route.js
+++ app/api/partners/route.js
@@ -2,27 +2,8 @@ import connectMongoDB from "../../../libs/mongodb";
 import Partner from "../../../models/partner";
 import { NextResponse } from "next/server";
 
-export async function GET(request) {
-  try {
-    // Connect to MongoDB
-    await connectMongoDB();
-
-    // Fetch partners from the database
-    const partners = await Partner.find();
-
-    // Prepare the response
-    const response = NextResponse.json(partners);
-
-    // Set cache control headers to prevent caching
-    response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
-    response.headers.set('Pragma', 'no-cache');
-    response.headers.set('Expires', '0');
-    response.headers.set('Surrogate-Control', 'no-store');
-
-    return response;
-  } catch (error) {
-    // Handle errors
-    console.error("Error fetching partners:", error);
-    return new Response("Internal Server Error", { status: 500 });
-  }
+export async function GET() {
+  await connectMongoDB();
+  const partners = await Partner.find();
+  return NextResponse.json( partners );
 }
